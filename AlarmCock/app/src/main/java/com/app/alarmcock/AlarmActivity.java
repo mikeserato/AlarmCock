9a23d424330d14150167dd7f45769a101d38de40
santosnarom <santosnarom@gmail.com>
2017-05-25 01:42:02 +0800

    added initial enable alarm functionality

--- AlarmCock/app/src/main/java/com/app/alarmcock/AlarmActivity.java
+++ AlarmCock/app/src/main/java/com/app/alarmcock/AlarmActivity.java
@@ -1,13 +1,59 @@
 package com.app.alarmcock;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class AlarmActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class AlarmActivity extends Activity {
+
+    private MediaPlayer mMediaPlayer;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_alarm);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_alarm_shake);
+
+        playSound(this, getAlarmUri());
+    }
+
+    private void playSound(Context context, Uri alert) {
+        mMediaPlayer = new MediaPlayer();
+        try {
+            mMediaPlayer.setDataSource(context, alert);
+            final AudioManager audioManager = (AudioManager) context
+                    .getSystemService(Context.AUDIO_SERVICE);
+            if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
+                mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+                mMediaPlayer.prepare();
+                mMediaPlayer.start();
+            }
+        } catch (IOException e) {
+            System.out.println("OOPS");
+        }
+    }
+
+    private Uri getAlarmUri() {
+        Uri alert = RingtoneManager
+                .getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alert == null) {
+            alert = RingtoneManager
+                    .getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            if (alert == null) {
+                alert = RingtoneManager
+                        .getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            }
+        }
+        return alert;
     }
 }
--- AlarmCock/app/src/main/java/com/app/alarmcock/MainActivity.java
+++ AlarmCock/app/src/main/java/com/app/alarmcock/MainActivity.java
@@ -1,12 +1,18 @@
 package com.app.alarmcock;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-        implements AlarmListFragment.OnAlarmSelected {
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements AlarmListFragment.OnAlarmSelected {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +41,18 @@ public class MainActivity extends AppCompatActivity
     public void onAddAlarmClick(View view) {
         Intent intent = new Intent(this, AddAlarm.class);
         startActivity(intent);
+    }
+
+    public void enableAlarm(View view) {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, 5);
+
+        Intent intent = new Intent(this, AlarmActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                12345, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        AlarmManager am =
+                (AlarmManager)getSystemService(Activity.ALARM_SERVICE);
+        am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                pendingIntent);
     }
 }
